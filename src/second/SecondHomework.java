5c57a96e065fd31388a107aab88096427aa7360b
Nikita Shcherbyna <CherbinaNikita@gmail.com>
2022-08-01 14:07:59 +0300

    task 1 has been corrected

--- src/second/SecondHomework.java
+++ src/second/SecondHomework.java
@@ -1,36 +1,40 @@
 package second;
 
 
-import java.util.Locale;
-
 public class SecondHomework {
     public static void main(String[] args) {
-        wordsCount("I love Java!");
 
+        wordsCount("   I    love Java");
         String word = "EyE";
-        if (isWordPalindrome(word.toLowerCase())){
+        if (isWordPalindrome(word.toLowerCase())) {
             System.out.println(word.toLowerCase() + " -> palindrome");
-        }else{System.out.println(word.toLowerCase() + " -> is not a palindrome");
+        } else {
+            System.out.println(word.toLowerCase() + " -> is not a palindrome");
         }
 
     }
 
-   public static void wordsCount(String name) {
-        String[] words = name.split(" ");
-        System.out.println(name + "-> " + words.length + " words");
+    public static void wordsCount(String name) {
+        int count = 0;
+        for (int i = 0; i < name.length(); i++) {
+            if (name.charAt(i) == ' ' && name.charAt(i + 1) != ' ') {
+                count++;
+            }
+
+        }
+        System.out.println(name + " -> " + count + " words");
     }
 
-    public static boolean isWordPalindrome(String name){
+    public static boolean isWordPalindrome(String name) {
         char[] word = name.toCharArray();
         int left = 0;
-        int right = name.length()-1;
-        while(left != right && left < right){
+        int right = name.length() - 1;
+        while (left != right && left < right) {
             if (word[left] != word[right]) return false;
             left++;
             right--;
-        }return true;
-
-
+        }
+        return true;
 
     }
 
